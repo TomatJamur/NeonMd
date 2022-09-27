8ced376d2956852d37725320891a8077c6b0d1a9
TomatJamur <112707853+TomatJamur@users.noreply.github.com>
2022-09-27 08:04:49 +0700

    Update info-biodata.js

--- plugins/info-biodata.js
+++ plugins/info-biodata.js
@@ -3,11 +3,11 @@ let fs = require('fs')
 let handler = async (m) => {
 let krizyn = `
 •·––––––––––––––––––––––––––·•
-❑ *Nama* : Dani Ganz
-❑ *Umur* : 15 Tahun
-❑ *Kelas* : 9 SMP
-❑ *Ulang Tahun* : 16 DESEMBER 2007
-❑ *Alamat* : Indonesia, Kalimantan Selatan, Kandangan
+❑ *Nama* : Agiel
+❑ *Umur* : 17 Tahun
+❑ *Kelas* : 12 SMA
+❑ *Ulang Tahun* : Cari tau sendiri
+❑ *Alamat* : Indonesia, Kalimantan Barat, Pontianak
 •·––––––––––––––––––––––––––·•
 	`.trim()
   let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(fotonya7)).buffer()}, { upload: conn.waUploadToServer }) 
